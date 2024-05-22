669469b92a79e1594ae0bc1091925f8612081c2e
ValeriaMirza <valeriamirza5@gmail.com>
2024-05-22 14:42:32 +0300

    Add function to send emails with SendGrid in new file email_sender.py

---
+++ email_sender.py
@@ -0,0 +1,25 @@
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from database import save_to_database
+
+SENDGRID_API_KEY = ''
+FROM_EMAIL = 'tasknotifier0@gmail.com'
+
+def send_email(to_email, subject, content):
+    message = Mail(
+        from_email=FROM_EMAIL,
+        to_emails=to_email,
+        subject=subject,
+        plain_text_content=content + 'hello',
+        html_content=f'<p>{content}</p>'
+    )
+
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        if response.status_code == 202:
+            return True, "Email accepted for delivery."
+        else:
+            return False, f"Failed to send email. Status code: {response.status_code}"
+    except Exception as e:
+        return False, f"Error sending email: {e}"
